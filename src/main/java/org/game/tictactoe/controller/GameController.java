6ceae493188c60fcb6ee10db8d8f1ebd93ea5d6e
Kyrylo Tsuper <ktsuper13@gmail.com>
2021-03-21 23:43:43 +0200

    UI bug fixes; Added proper handling of missing parameters exception; Added postman collection.

--- src/main/java/org/game/tictactoe/controller/GameController.java
+++ src/main/java/org/game/tictactoe/controller/GameController.java
@@ -1,7 +1,10 @@
 package org.game.tictactoe.controller;
 
 import io.swagger.annotations.ApiOperation;
+import org.game.tictactoe.exceptions.GameBadRequestException;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,5 +22,10 @@ public class GameController {
     )
     public String game(@RequestParam String board, @RequestParam(name = "game_id") String gameId) {
         return "game";
+    }
+
+    @ExceptionHandler (MissingServletRequestParameterException.class)
+    public void handleMissingParams(MissingServletRequestParameterException ex) {
+        throw new GameBadRequestException("Missing required parameter - " + ex.getParameterName());
     }
 }
